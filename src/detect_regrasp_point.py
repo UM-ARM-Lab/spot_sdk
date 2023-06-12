b41f141ade2e08b53a9b983bd4300bb00edb2cbd
Riley <alison.ryckman@gmail.com>
2023-06-12 11:30:13 -0400

    coded up new perception functions, ready to try integrating

--- src/detect_regrasp_point.py
+++ src/detect_regrasp_point.py
@@ -1,26 +1,3 @@
-"""
-
-This script shows how to detect the grasp point in an image using the segmentation model trained with roboflow.
-The basic algorithm is:
-1. Load the image and the predictions from the model
-2. Find candidate points, which are points on the rope that are near but not inside an obstacle
-3. Find the candidate point that is closest to the bottom center of the image (closest to the robot)
-
-Within a larger script, you might use it like this:
-
-```python
-    from detect_regrasp_point import init_roboflow, detect_regrasp_point
-
-    # initialize once at the top
-    model = init_roboflow()
-
-    # Then call this function for each image.
-    # You need to save the image to a file first, in this case "latest_hand_img.jpg"
-    test_image_filename = "latest_hand_img.jpg"
-    detection = detect_regrasp_point(model, test_image_filename)
-```
-
-"""
 import json
 import os
 from dataclasses import dataclass
@@ -34,10 +11,7 @@ import numpy as np
 from PIL import Image
 from roboflow import Roboflow
 
-OBSTACLE_NEAR_PX = 10
-
 MIN_CONFIDENCE = 0.25
-OBSTACLE_INFLATION_PX = 50
 
 
 class DetectionError(Exception):
@@ -52,93 +26,11 @@ class DetectionResult:
     img: Image.Image
 
 
-def main():
-    model = init_roboflow()
-
-    test_image_filename = "test3.jpg"
-    detection = detect_regrasp_point(model, test_image_filename)
-
-    plt.figure()
-    plt.imshow(detection.img)
-    rng = np.random.RandomState(0)
-    for pred in detection.predictions:
-        points = pred["points"]
-        x = [p['x'] for p in points]
-        y = [p['y'] for p in points]
-        plt.plot(x, y, c=cm.hsv(rng.uniform()), linewidth=2, zorder=1)
-    plt.scatter(detection.candidates_pxs[:, 0], detection.candidates_pxs[:, 1], color="y", marker="x", s=100,
-                label='candidates',
-                zorder=2)
-    plt.scatter(detection.grasp_px[0], detection.grasp_px[1], color="green", marker="o", s=100, label='grasp point',
-                zorder=3)
-    plt.legend()
-    plt.show()
-
-
-def detect_regrasp_point(model, test_image_filename):
-    img = Image.open(test_image_filename)
-    predictions = get_or_load_predictions(model, test_image_filename)
-    predictions = predictions["predictions"]
-    rope_class_name = "blue_rope"
-    obstacle_class_name = "battery"
-    # create masks based on the polygons for each class
-    rope_masks = []
-    obstacle_masks = []
-    rope_polygons = []
-    obstacle_polygons = []
-    for pred in predictions:
-        if pred['confidence'] < MIN_CONFIDENCE:
-            continue
-        class_name = pred["class"]
-        points = pred["points"]
-        points = np.array([(p['x'], p['y']) for p in points], dtype=int)
-        mask = np.zeros([img.height, img.width], dtype=np.uint8)
-        cv2.fillPoly(mask, [points], 255)
-
-        if class_name == rope_class_name:
-            rope_polygons.append(points)
-            rope_masks.append(mask)
-        elif class_name == obstacle_class_name:
-            obstacle_polygons.append(points)
-            obstacle_masks.append(mask)
-    if len(obstacle_masks) == 0:
-        raise DetectionError("No obstacles detected")
-    if len(rope_masks) == 0:
-        raise DetectionError("No rope detected")
-    # # combine the masks for each class into a single mask
-    # rope_mask = combine_masks(img, rope_masks)
-    # obstacle_mask = combine_masks(img, obstacle_masks)
-    # # inflate the obstacle mask so that the detected grasp point isn't so close to the object that we can't grasp it
-    # obstacle_mask = cv2.dilate(obstacle_mask, np.ones((50, 50), np.uint8))
-    candidates_pxs = []
-    for rope_poly in rope_polygons:
-        for rope_p in rope_poly:
-            for obstacle_poly in obstacle_polygons:
-                dist = cv2.pointPolygonTest(obstacle_poly, rope_p.tolist(), True)
-                # if the point is inside the obstacle, we can't grasp it
-                if abs(dist + OBSTACLE_INFLATION_PX) < OBSTACLE_NEAR_PX:
-                    candidates_pxs.append(rope_p)
-    candidates_pxs = np.array(candidates_pxs)
-    # compute the distance to the camera, approximated by the distance to the center of th image
-    # for each candidate point.
-    d_to_center = np.linalg.norm(candidates_pxs - np.array([img.width / 2, img.height / 2]), axis=-1)
-    grasp_px = candidates_pxs[np.argmin(d_to_center)]
-
-    return DetectionResult(grasp_px, candidates_pxs, predictions, img)
-
-
 def init_roboflow():
     rf = Roboflow(api_key=os.environ['ROBOFLOW_API_KEY'])
-    project = rf.workspace().project("robotgardening")
-    model = project.version(6).model
+    project = rf.workspace().project("spot-vaccuming-demo")
+    model = project.version(8).model
     return model
-
-
-def combine_masks(img, masks):
-    combined_mask = np.zeros([img.height, img.width], dtype=np.uint8)
-    for mask in masks:
-        combined_mask = cv2.bitwise_or(combined_mask, mask)
-    return combined_mask
 
 
 def get_or_load_predictions(model, test_image_filename):
@@ -155,6 +47,111 @@ def get_or_load_predictions(model, test_image_filename):
             json.dump(predictions, f)
 
     return predictions
+
+
+def viz_detection(detection):
+    plt.figure()
+    plt.imshow(detection.img)
+    rng = np.random.RandomState(0)
+    class_colors = {}
+    for pred in detection.predictions:
+        points = pred["points"]
+        class_name = pred["class"]
+        if class_name not in class_colors:
+            class_colors[class_name] = cm.hsv(rng.uniform())
+        x = [p['x'] for p in points]
+        y = [p['y'] for p in points]
+        c = class_colors[class_name]
+        plt.plot(x, y, c=c, linewidth=2, zorder=1)
+    plt.scatter(detection.candidates_pxs[:, 0], detection.candidates_pxs[:, 1], color="y", marker="x", s=100,
+                label='candidates',
+                zorder=2)
+    plt.scatter(detection.grasp_px[0], detection.grasp_px[1], color="green", marker="o", s=100, label='grasp point',
+                zorder=3)
+    plt.legend()
+    plt.show()
+
+
+def get_polys(predictions, desired_class_name):
+    polys = []
+    for pred in predictions:
+        if pred['confidence'] < MIN_CONFIDENCE:
+            continue
+        class_name = pred["class"]
+        points = pred["points"]
+        points = np.array([(p['x'], p['y']) for p in points], dtype=int)
+
+        if class_name == desired_class_name:
+            polys.append(points)
+    return polys
+
+
+def get_predictions(model, test_image_filename):
+    img = Image.open(test_image_filename)
+    predictions = get_or_load_predictions(model, test_image_filename)
+    predictions = predictions["predictions"]
+    return img, predictions
+
+
+def detect_regrasp_point(img, predictions, near_px, far_px):
+    hose_class_name = "vacuum_hose"
+    obstacle_class_name = "battery"
+
+    hose_polygons = get_polys(predictions, hose_class_name)
+    obstacle_polygons = get_polys(predictions, obstacle_class_name)
+
+    candidates_pxs = []
+    for hose_poly in hose_polygons:
+        for hose_p in hose_poly:
+            min_d_to_any_obstacle = np.inf
+            for obstacle_poly in obstacle_polygons:
+                # dist is positive if the point is outside the polygon
+                dist = -cv2.pointPolygonTest(obstacle_poly, hose_p.tolist(), True)
+                if dist < min_d_to_any_obstacle:
+                    min_d_to_any_obstacle = dist
+            # a point is a candidate if it is within some distance of an obstacle
+            if near_px < min_d_to_any_obstacle < far_px:
+                candidates_pxs.append(hose_p)
+    candidates_pxs = np.array(candidates_pxs)
+    # compute the distance to the camera, approximated by the distance to the bottom-center of th image
+    # for each candidate point.
+    d_to_center = np.linalg.norm(candidates_pxs - np.array([0, img.height / 2]), axis=-1)
+    grasp_px = candidates_pxs[np.argmin(d_to_center)]
+
+    return DetectionResult(grasp_px, candidates_pxs, predictions, img)
+
+
+def detect_object_center(img, predictions, class_name):
+    polygons = get_polys(predictions, class_name)
+
+    if len(polygons) == 0:
+        raise DetectionError("No vacuum head detected")
+    if len(polygons) > 1:
+        raise DetectionError("Multiple vacuum heads detected")
+
+    poly = polygons[0]
+    M = cv2.moments(poly)
+    cx = int(M["m10"] / M["m00"])
+    cy = int(M["m01"] / M["m00"])
+    grasp_point = np.array([cx, cy])
+
+    return DetectionResult(grasp_point, [grasp_point], predictions, img)
+
+
+def main():
+    model = init_roboflow()
+
+    test_image_filename = "test1.png"
+    img, predictions = get_predictions(model, test_image_filename)
+
+    regrasp_detection = detect_regrasp_point(img, predictions, 10, 50)
+    viz_detection(regrasp_detection)
+
+    head_detection = detect_object_center(img, predictions, "vacuum_head")
+    viz_detection(head_detection)
+
+    mess_detection = detect_object_center(img, predictions, "mess")
+    viz_detection(mess_detection)
 
 
 if __name__ == "__main__":
